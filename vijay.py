77009f68a3905ae73457fcfd0b5c7e41a6bd30cc
vijayasharthi <96008995+vijayasharthi@users.noreply.github.com>
2022-02-28 23:04:52 +0530

    Add files via upload

---
+++ vijay.py
@@ -0,0 +1,10 @@
+x=("welcome")
+print(x)
+a=int(input("Enter your age :"))
+if(a>=18):
+    print("Elegible for voting")
+else:
+    print("Not eligible for voting")
+print("Thank you")    
+    
+    
