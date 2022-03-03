4702f8e8dad34199f37d1e854886acc25b35dfaa
Vijayasharathi <96008995+vijayasharthi@users.noreply.github.com>
2022-03-03 21:50:05 +0530

    Add files via upload

---
+++ elif ladder.py
@@ -0,0 +1,7 @@
+a=int(input("Enter your number "))
+if(a>0):
+    print("The given number is positive number")
+elif(a<0):
+    print("The given number is negative number")
+elif(a==0):
+    print("The given number is zero")
