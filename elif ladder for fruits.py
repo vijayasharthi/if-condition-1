9306e210fc3b217b8e34513698a520f883292180
Vijayasharathi <96008995+vijayasharthi@users.noreply.github.com>
2022-03-03 22:35:36 +0530

    Add files via upload

---
+++ elif ladder for fruits.py
@@ -0,0 +1,12 @@
+a=input("Enter your fruit:")
+print(a)
+if(a=="Mango"):
+    print("You entered a mango")
+elif(a=="grapes"):
+    print("You entered a grapes")
+elif(a=="orange"):
+    print("You entered a orange")
+else:
+     print(str(a +" is not available"))
+    
+    
