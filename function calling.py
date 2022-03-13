f8f515ffef584d1758630cab228ca14dc70938d6
Vijayasharathi <96008995+vijayasharthi@users.noreply.github.com>
2022-03-13 20:03:10 +0530

    Add files via upload

---
+++ function calling.py
@@ -0,0 +1,16 @@
+a=int(input("Enter the number :"))
+b=int(input("Enter the number :"))
+def add(a,b):
+    return(a+b)
+def divide(a,b):
+    print(a/b)
+def sub(a,b):
+    return(a-b)
+def multiply(a,b):
+    return(a*b)
+
+print(add(a,b))
+print(sub(a,b))
+print(multiply(a,b))
+divide(a,b)
+
